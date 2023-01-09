9478fed24f7538b5ebc1bea343bc263d47b1cb3f
John Lopez <Lopez.thinktank@gmail.com>
2023-01-08 19:54:00 -0800

    UPdated multiple files due to merge duplicating some files, deleted some folders. Index.js file was updated with new code products,categories, and tags.

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,21 @@
 const { Model, DataTypes } = require('sequelize');
 
-const sequelize = require('../config/connection.js');
+const sequelize = require('../../config/connection.js');
 
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      promaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
